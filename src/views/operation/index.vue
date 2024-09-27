fcd500adf674fc60979cfaeeffbf74bcf8c090f5
xuz <xuz@wanthings.com>
2024-09-27 14:53:07 +0800

    ✨ Feat: 配置

---
+++ src/views/operation/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="operation-container">
+    <!-- -->
+  </div>
+</template>
+
+<script setup name="operation" lang="ts">
+
+</script>
+<style scoped>
+.operation-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
